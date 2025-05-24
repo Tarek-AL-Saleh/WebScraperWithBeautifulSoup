26f631baebdaf35e700276364126b82c38c59c6d
Tarek-AL-Saleh <tarek.z.alsaleh@gmail.com>
2025-05-24 17:55:28 +0300

    task 3

--- data_cleaner.py
+++ data_cleaner.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 data=pd.read_csv("output.csv")
 
-data['Price']=data['Price'].replace('\£','',regex=True).astype(float)
+data['Price']=data['Price'].replace('£','',regex=True).astype(float)
 
 data['Price_Caterogy']=data['Price'].apply(lambda x: 'Cheap' if x<40 else 'Expensive')
 print(data[['Price','Price_Caterogy']].head())
 
-data.to_csv('cleaned_output.csv',index=False)
+data.to_csv('cleaned_output1.csv',index=False)
